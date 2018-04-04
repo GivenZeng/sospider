169eb84729e4a4e72d50e52b6481efc6924def8a
Given <zenggiven@gmail.com>
2018-04-04 15:39:17 +0800

    try cache

--- sospider/main.py
+++ sospider/main.py
@@ -18,26 +18,29 @@ def main():
     for name in names:
         authors = api.search_author(name)
         for author in authors:
-            # 已经处理过该author
-            if author['id'] in dealt_authors:
-                continue
-            dealt_authors[author['id']] = True
-            # 获取用户信息
-            info = api.get_author(author['id'])
-            db.upsert_author(info)
+            try:
+                # 已经处理过该author
+                if author['id'] in dealt_authors:
+                    continue
+                dealt_authors[author['id']] = True
+                # 获取用户信息
+                info = api.get_author(author['id'])
+                db.upsert_author(info)
 
-            # 查询合作者的信息
-            for co in info['co-authors']:
-                if co['id'] in dealt_authors:
-                    continue
-                util.start_get_author_thread(co['id'])
-            # 查询paper
-            for p in info['papers']:
-                # 判断该paper是否已经查询
-                if p['id'] in dealt_paper:
-                    continue
-                dealt_paper[p['id']] = True
-                util.start_get_paper_thread(p['id'], author['id'])
+                # 查询合作者的信息
+                for co in info['co-authors']:
+                    if co['id'] in dealt_authors:
+                        continue
+                    util.start_get_author_thread(co['id'])
+                # 查询paper
+                for p in info['papers']:
+                    # 判断该paper是否已经查询
+                    if p['id'] in dealt_paper:
+                        continue
+                    dealt_paper[p['id']] = True
+                    util.start_get_paper_thread(p['id'], author['id'])
+            except:
+                logger.debug('get error in a author query')
             # 每个author停止2s
             time.sleep(1)
         # 每个name停止10s
